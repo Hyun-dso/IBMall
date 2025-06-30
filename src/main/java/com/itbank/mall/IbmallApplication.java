2a0fe27a35c377b5a30512dba37d80c95dd3f13a
Hyun-dso <dltkdgus0413@gmail.com>
2025-06-30 17:41:45 +0900

    Itbank Mall start
    Itbank Mall start

---
+++ src/main/java/com/itbank/mall/IbmallApplication.java
@@ -0,0 +1,13 @@
+package com.itbank.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbmallApplication.class, args);
+	}
+
+}
