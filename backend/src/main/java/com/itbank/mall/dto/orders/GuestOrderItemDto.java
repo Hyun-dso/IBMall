bf418bc4dcecd2cc066b2410d8ece579753417e5
Hyun-dso <dltkdgus0413@gmail.com>
2025-08-19 18:19:44 +0900

    merge 과정 중 제거된 내용 복구
    merge 과정 중 제거된 내용 복구

--- backend/src/main/java/com/itbank/mall/dto/orders/GuestOrderItemDto.java
+++ backend/src/main/java/com/itbank/mall/dto/orders/GuestOrderItemDto.java
@@ -7,4 +7,6 @@ public class GuestOrderItemDto {
     private Long productId;
     private Long productOptionId;
     private String productName;
+    private int quantity;
+    private int price;
 }
