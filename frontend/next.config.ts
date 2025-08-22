9cd1681bbd0aa88dece919a5b0696b82a5a869eb
Hyun-dso <dltkdgus0413@gmail.com>
2025-08-22 17:21:11 +0900

    프론틍덴드 devlopment 삭제
    프론틍덴드 devlopment 삭제

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,14 +2,12 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    if (process.env.NODE_ENV === 'development') {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://localhost:8080/api/:path*',
-        },
-      ]
-    }
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*',
+      },
+    ]
     // 운영 환경에서는 Nginx/ALB가 proxy_pass 처리
     return []
   },
